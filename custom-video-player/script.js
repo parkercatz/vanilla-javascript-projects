1101cc9932f342f70f0a7e099bf840a2c722c986
ParkerCatz <tksm.webdesign09@gmail.com>
2021-05-28 22:12:16 +0900

    feat: progress bar & timestamp

--- custom-video-player/script.js
+++ custom-video-player/script.js
@@ -1,12 +1,12 @@
-const video = document.createElementId("video");
-const play = document.createElementId("play");
-const stop = document.createElementId("stop");
-const progress = document.createElementId("progress");
-const timestamp = document.createElementId("timestamp");
+const video = document.getElementById("video");
+const play = document.getElementById("play");
+const stop = document.getElementById("stop");
+const progress = document.getElementById("progress");
+const timestamp = document.getElementById("timestamp");
 
 // 再生 & 停止
 function toggleVideoStatus() {
-  if (vudeo.paused) {
+  if (video.paused) {
     video.play();
   } else {
     video.pause();
@@ -15,30 +15,49 @@ function toggleVideoStatus() {
 
 // 再生 / 一時停止アイコン
 function updatePlayIcon() {
-  return true;
+  if (video.paised) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+  }
 }
 
 // 進み & 時間
 function updateProgress() {
-  return true;
+  progress.value = (video.currentTime / video.duration) * 100;
+
+  // 分の取得
+  let mins = Math.floor(video.currentTime / 60);
+  if (mins < 10) {
+    mins = "0" + String(mins);
+  }
+
+  // 秒の取得
+  let secs = Math.floor(video.currentTime % 60);
+  if (secs < 10) {
+    secs = "0" + String(secs);
+  }
+
+  timestamp.innerHTML = `${mins}:${secs}`;
 }
 
 // ビデオ時間の進みを設定
 function setVideoProgress() {
-  return true;
+  video.currentTime = (+progress.value * video.duration) / 100;
 }
 
 // ビデオを停止
 function stopVideo() {
-  return true;
+  video.currentTime = 0;
+  video.pause();
 }
 
 // イベントリスナー
 video.addEventListener("click", toggleVideoStatus);
 video.addEventListener("pause", updatePlayIcon);
 video.addEventListener("play", updatePlayIcon);
-video.addEventListener("timeupdate", updateProgree);
+video.addEventListener("timeupdate", updateProgress);
 
 play.addEventListener("click", toggleVideoStatus);
 stop.addEventListener("click", stopVideo);
-progress.addEventListener("click", setVideoProgress);
+progress.addEventListener("change", setVideoProgress);
