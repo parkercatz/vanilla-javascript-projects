94e8106577cec19e5670efa26f66ceb547f15373
ParkerCatz <tksm.webdesign09@gmail.com>
2021-05-31 23:42:55 +0900

    feat: updateDOM & format number & addEventListner

--- dom-array-methods/script.js
+++ dom-array-methods/script.js
@@ -8,8 +8,6 @@ const calculateWealthBtn = document.getElementById("calculate-wealth");
 let data = [];
 
 getRandomUser();
-getRandomUser();
-getRandomUser();
 
 // fetch random user and add money
 async function getRandomUser() {
@@ -19,7 +17,7 @@ async function getRandomUser() {
   const user = data.results[0];
 
   const newUser = {
-    name: `${user.name.first} ${user.name.list}`,
+    name: `${user.name.first} ${user.name.last}`,
     money: Math.floor(Math.random() * 1000000)
   };
 
@@ -29,4 +27,29 @@ async function getRandomUser() {
 // Add new obj to data arr
 function addData(obj) {
   data.push(obj);
+
+  updateDOM();
 }
+
+// update DOM
+function updateDOM(providedData = data) {
+  // Clear dmain div
+  main.innerHTML = "<h2><strong>Person</strong> Wealth</h2>";
+
+  providedData.forEach(item => {
+    const element = document.createElement("div");
+    element.classList.add("person");
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+      item.money
+    )}`;
+    main.appendChild(element);
+  });
+}
+
+// Format number as money
+function formatMoney(number) {
+  return "$" + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
+}
+
+// イベントリスナー
+addUserButton.addEventListener("click", getRandomUser);
